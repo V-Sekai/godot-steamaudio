b2fcaaf01cc9ad42cd993049528808e865e99a8c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-02-19 22:41:19 -0800

    Apply style changes.

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 # config.py
 
+
 def can_build(env, platform):
     if env.msvc:
         print("Error: Cannot build with Steam Audio MSVC.")
         return False
     return True
 
+
 def configure(env):
     pass
-
