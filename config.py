40fb2c6eaf52b0306491a01a6eef102e89926c55
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-02-19 23:17:37 -0800

    Update platform check to include "web" and "android".

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 
 def can_build(env, platform):
-    if platform == "macos":
+    if platform in ["web", "android", "macos"]:
         return False
     if env.msvc:
         print("Error: Cannot build with Steam Audio MSVC.")
