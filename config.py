2ddc69350a5defb00b59e5090884022805256885
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-02-19 23:15:02 -0800

    Prevent building on macOS in can_build function
    Added a condition to prevent building on macOS in the can_build function.

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 
 
 def can_build(env, platform):
+    if platform == "macos":
+        return False
     if env.msvc:
         print("Error: Cannot build with Steam Audio MSVC.")
         return False
