356c2f73e60d0b12faefd43d226e71439caac157
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-02-20 06:41:41 -0800

    Link with msvc.

--- config.py
+++ config.py
@@ -4,9 +4,6 @@
 def can_build(env, platform):
     if platform in ["web", "android", "macos"]:
         return False
-    if env.msvc:
-        print("Error: Cannot build with Steam Audio MSVC.")
-        return False
     return True
 
 
